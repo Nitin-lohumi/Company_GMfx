3449f97be1b875193e91bcaf5cab1894eb0f739d
Nitin-Lohumi <lohuminitin@gmail.com>
2025-03-10 12:10:31 +0530

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name="google-adsense-account" content="ca-pub-4592442373021387" />
-        <script
+        <meta name="google-site-verification" content="ca-pub-4592442373021387" />
+        {/* <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4592442373021387"
           crossorigin="anonymous"
-        ></script>
+        ></script> */}
       </Head>
       <body>
         <div className="background-overlay"></div>
